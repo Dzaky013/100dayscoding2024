4083510cd870d1363c2336a09a6d11b5f3cd58fc
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-05 11:45:46 +0800

    Create Day004.java

---
+++ Day004.java
@@ -0,0 +1,11 @@
+package Day04;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Halo Perkenalkan");
+        System.out.printf("Nama saya %s dan saya berumur %d tahun.", "Dzaky Surya Darma", 19);
+        
+         }
+    
+}
