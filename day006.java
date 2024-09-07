fc4c000c19d3a352e889edd93922d162d4a9f5c3
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-07 10:08:23 +0800

    Create day006.java

---
+++ day006.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String nama;
+        int umur;
+        char gender; 
+      //String untuk teks
+      //int untuk bilangan bulat
+      //char untuk karakter/gender 
+      }
+}
