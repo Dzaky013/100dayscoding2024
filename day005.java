6f45d7b8599c670996ce4c45ed16800baba9a9d2
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-07 09:43:52 +0800

    Create day005.java

---
+++ day005.java
@@ -0,0 +1,18 @@
+public class main {
+			public static void main(String[] args) {
+			System.out.println("Ohayo minasan");
+			//println digukan unruk mencetak data dan perpindahan baris baru
+			System.out.print("Hajimemashte ");
+			//print digunakan untuk mencetak data tanpa perpindahan baris baru
+			System.out.printf("Watashi no namaewa %s %d-saidesu","Dzaky Surya Darma", 19);
+			/*printf digunakan untuk menampilkan suatu keluaran pada layar berdasarkan suatu penentu format
+			*%s untuk string (teks) %d untuk anka (variabel) atau dseimal
+			*/
+		
+		System.out.println(" Watashi wa pamboang ni sunde imasu");
+		//komnetar ini hanya untuk 1 kolom
+		/*komentar ini digunalan untuk lebih dari 1 kolom*/
+		
+		}
+
+}
