3a529a1e43d635e320aed2067bc6fa9beb063d0b
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-08 16:29:06 +0800

    Create Day007.java

---
+++ Day007.java
@@ -0,0 +1,19 @@
+package day007;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Inisialisasi variabel dengan nilai default
+        String nama = "Dzaky Surya Darma";  
+        char kelas = 'A';
+        int umur = 19;
+        String alamat = "Pamboang";
+        char gender = 'L'; 
+        // Menampilkan nilai variabel
+        System.out.println("nama:" + nama);
+        System.out.println("kelas:" + kelas);
+        System.out.println("umur:" + umur);
+        System.out.println("gender:" + gender);  
+    }
+}
