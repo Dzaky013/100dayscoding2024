5777b4f4fff422b09bd3f3843bf5b5fdf7aa6307
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-09 20:09:44 +0800

    Create Day008.java

---
+++ Day008.java
@@ -0,0 +1,27 @@
+package day08;
+
+public class Main {
+    
+    public static void main(String[] args) {
+     
+//Sebelum di update
+        String nama = "saki";
+        System.out.println(nama);
+        // Sesudah di update
+        nama = "Dzaky Surya Darma";
+        System.out.println(nama); 
+        int umur = 18;
+        System.out.println(umur);
+        umur = 19;
+        System.out.println(umur);
+        String alamat = "pamboang";
+        System.out.println(alamat);
+        alamat = "bulutupang";
+        System.out.println(alamat);
+        char gender='L';
+        System.out.println(gender);
+        gender ='M';
+        System.out.println(gender);
+    }
+    
+}
