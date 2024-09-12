431221dbfdf91249f580b24a455421dd320462e5
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-12 11:42:55 +0800

    Create day010.java

---
+++ day010.java
@@ -0,0 +1,11 @@
+package day010;
+public class Main {
+   //ini public class
+    public static void main(String[] args) {
+       //ini method
+        byte ukuran = 43;
+         //byte digunakan untuk bilangan desimal
+    System.out.println("Ukuran sepatu = " + ukuran + "cm");
+    // System out ini untuk memanggil type data byte yang ada di atas
+        }
+}
