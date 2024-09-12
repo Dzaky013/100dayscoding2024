028ab1cebf12a39ac190fbbcf7154a796c97d56c
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-12 11:45:02 +0800

    Create day011.java

---
+++ day011.java
@@ -0,0 +1,13 @@
+package day11;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        short uang = 1000;
+        /*tipe data short untuk bilangan bulat 
+        maksimum short java -32768 sampai 32767*/
+        System.out.println("Kembalian uang = Rp." + uang);
+        /*Sytem ini untuk menampilkan output variabel yang 
+        sudah di buat*/
+    }
+}
