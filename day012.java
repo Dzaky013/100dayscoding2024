69c4a5c08396144d32344ab15aaad3da3ef76bb4
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-14 18:43:59 +0800

    Create day012.java

---
+++ day012.java
@@ -0,0 +1,15 @@
+package day012;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int jumlah = 29;
+        /*ini ada tipe data int, int digunakan
+         * untuk bilangan bulat batas maksimal
+         * untuk di int -2,147,483,648 hingga 2,147,483,647.
+         * */
+        System.out.println("Anggota kelas saya = " + jumlah);
+        //ini untuk mencetak atau memprint 
+    }
+    
+}
