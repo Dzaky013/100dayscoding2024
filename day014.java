b11fc4d905c390bb27b509a618ca27a453a340ef
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-15 21:04:57 +0800

    Create day014.java

---
+++ day014.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        long tinggi;
+        tinggi = 167;
+        System.out.println("Tinggi badan saya = " + tinggi + "cm");
+    }
+}
