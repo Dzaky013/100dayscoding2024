5a4c2c5d231ed481f40a7683ae104f255c9b083a
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-16 23:42:18 +0800

    Create Day015.java

---
+++ Day015.java
@@ -0,0 +1,11 @@
+package day15;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        double berat ;
+        berat = 67.56;
+        System.out.println("berat badan saya = " + berat + "kg");
+    }
+    
+}
