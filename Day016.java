0f5f2b412699571a52110919573250fd324ab76d
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-17 21:30:34 +0800

    Create Day016.java

---
+++ Day016.java
@@ -0,0 +1,12 @@
+package day16;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        float ukuran ; 
+        ukuran = 30.0f;
+        System.out.println("panjang penggaris besi = " + ukuran + "cm");
+        
+    }
+    
+    }
