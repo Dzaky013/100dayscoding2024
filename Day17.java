6ab714eb2cca27a0704d30bf39d301e77af5348d
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-18 21:42:42 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,19 @@
+package day17;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        char A = 'D';
+        char B = 'Z';
+        char C = 'A';
+        char D = 'K';
+        char E = 'Y';
+        System.out.print("Nama saya = ");
+        System.out.print(A);
+        System.out.print(B);
+        System.out.print(C);
+        System.out.print(D);
+        System.out.print(E);
+    }
+    
+    }
