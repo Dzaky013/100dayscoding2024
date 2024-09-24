b6a44afa37adf17272466a6b5864b5b4dc60e584
Dzaky013 <dzakysuryadarma@gmail.com>
2024-09-24 23:16:47 +0800

    Create Day023.java

---
+++ Day023.java
@@ -0,0 +1,11 @@
+package day023;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Long nomr = 10L;
+        System.out.println(nomr);
+        
+    }
+    
+          }
